8c7c2faed36b149dd6fea7ba08b6ee0f57159faf
Ilana Cabral <ilana.cabral@gmail.com>
2021-10-09 18:04:43 -0300

    first commit

---
+++ src/main/java/br/com/diolabs/childapi/ChildapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.diolabs.childapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChildapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/diolabs/childapi/ChildapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diolabs.childapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
