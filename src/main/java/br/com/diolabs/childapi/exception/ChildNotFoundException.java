28326a9281f618b345cd923cce9d92ec75376a29
Ilana Cabral <ilana.cabral@gmail.com>
2021-10-11 07:55:18 -0300

    Corrigindo response das exceções

--- src/main/java/br/com/diolabs/childapi/exception/ChildNotFoundException.java
+++ src/main/java/br/com/diolabs/childapi/exception/ChildNotFoundException.java
@@ -1,5 +1,9 @@
 package br.com.diolabs.childapi.exception;
 
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ChildNotFoundException extends RuntimeException{
 
     public ChildNotFoundException(Long id) {
